c210cadc1ddd18349ea451fff3097150775495cb
Manishjain92744 <manishkumar25031999@gmail.com>
2022-09-09 09:50:54 +0530

    basic auth is added

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 // Available routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/auth',require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 const express=require('express');
+const User = require('../models/User');
 const router=express.Router();
 
-router.get('/',(req,res)=>{
- obj={
-    a:'thios',
-    number:42
- }
- res.json(obj)
+//create user using post "/api/auth" . doesn't required auth 
+
+router.post('/',(req,res)=>{
+ console.log(req.body);
+ const user = User(req.body);
+ user.save()
+   res.send(req.body);
+ 
 })
 
 module.exports = router
